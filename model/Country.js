28502cb6b617f26d519c9f650679f3c7758ce8ec
Kien Nguyen <trungkien71297@gmail.com>
2019-06-18 15:38:05 +0700

    Model City, Country

---
+++ model/Country.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const db = require('../config/db');
+
+const Country = db.define('city', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    country_code: {
+        type: Sequelize.STRING
+    }, 
+    country_name: {
+        type: Sequelize.STRING
+    },   
+    
+}, {
+        timestamps: false,
+        freezeTableName: true
+    }
+);
+
+module.exports = Device;
