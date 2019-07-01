f3505a1260d1adacea30bd2d963004f75fafb14e
Thainkh <nguyenkimhungthai@gmail.com>
2019-07-02 05:42:36 +0700

    update audience v2

--- client/src/pages/audience/index.js
+++ client/src/pages/audience/index.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import Authorize from 'components/LayoutComponents/Authorize'
 import ChartistGraph from 'react-chartist'
-import ChartCard from 'components/Components/ChartCard'
+import { Tabs, Table, Menu, Dropdown, Icon } from 'antd'
+import ChartCard2 from 'components/Components/ChartCard2'
 import { Helmet } from 'react-helmet'
 import ChartistTooltip from 'chartist-plugin-tooltips-updated'
 import C3Chart from 'react-c3js'
+// import styles from './style.module.scss'
 
 const areaData = {
   labels: [1, 2, 3, 4, 5, 6, 7, 8],
   series: [[5, 9, 7, 8, 5, 3, 5, 4]],
 }
+
+const menu = (
+  <Menu>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+        1st menu item
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+        2nd menu item
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+        3rd menu item
+      </a>
+    </Menu.Item>
+  </Menu>
+)
 
 const colors = {
   primary: '#01a8fe',
@@ -18,6 +40,76 @@ const colors = {
   danger: '#fb434a',
 }
 
+const { TabPane } = Tabs
+
+const columns = [
+  {
+    title: 'Language',
+    dataIndex: 'language',
+    key: 'language',
+  },
+  {
+    title: 'Users',
+    dataIndex: 'users',
+    key: 'users',
+    width: '12%',
+  },
+  {
+    title: '% Users',
+    dataIndex: 'percentusers',
+    width: '30%',
+    key: 'percentusers',
+  },
+]
+const columns1 = [
+  {
+    title: 'Devices',
+    dataIndex: 'devices',
+    key: 'devices',
+  },
+  {
+    title: 'Users',
+    dataIndex: 'users',
+    key: 'users',
+    width: '12%',
+  },
+  {
+    title: '% Users',
+    dataIndex: 'percentusers',
+    width: '30%',
+    key: 'percentusers',
+  },
+]
+
+const data = [
+  {
+    key: 1,
+    language: 'vietnamese',
+    users: 60,
+    percentusers: '60%',
+  },
+  {
+    key: 2,
+    language: 'english',
+    users: 40,
+    percentusers: '40%',
+  },
+]
+
+const data2 = [
+  {
+    key: 1,
+    devices: 'laptop',
+    users: 60,
+    percentusers: '60%',
+  },
+  {
+    key: 2,
+    devices: 'PC',
+    users: 40,
+    percentusers: '40%',
+  },
+]
 const areaOptions = {
   low: 0,
   showArea: true,
@@ -34,7 +126,7 @@ const pie = {
   },
 }
 
-class Acquisition extends React.Component {
+class Audience extends React.Component {
   render() {
     return (
       <Authorize roles={['admin']}>
@@ -45,6 +137,13 @@ class Acquisition extends React.Component {
               <div className="card-header">
                 <h5 className="text-black">
                   <strong>Overview</strong>
+                  <br />
+                  <br />
+                  <Dropdown overlay={menu}>
+                    <a className="ant-dropdown-link" href="#">
+                      User <Icon type="down" />
+                    </a>
+                  </Dropdown>
                 </h5>
               </div>
               <div className="card-body">
@@ -62,7 +161,7 @@ class Acquisition extends React.Component {
         </div>
         <div className="row">
           <div className="col-md-2">
-            <ChartCard
+            <ChartCard2
               title="Users"
               amount="240"
               chartProps={{
@@ -81,7 +180,7 @@ class Acquisition extends React.Component {
             />
           </div>
           <div className="col-md-2">
-            <ChartCard
+            <ChartCard2
               title="Users"
               amount="240"
               chartProps={{
@@ -100,7 +199,7 @@ class Acquisition extends React.Component {
             />
           </div>
           <div className="col-md-2">
-            <ChartCard
+            <ChartCard2
               title="Users"
               amount="240"
               chartProps={{
@@ -119,7 +218,7 @@ class Acquisition extends React.Component {
             />
           </div>
           <div className="col-md-2">
-            <ChartCard
+            <ChartCard2
               title="Users"
               amount="240"
               chartProps={{
@@ -138,7 +237,7 @@ class Acquisition extends React.Component {
             />
           </div>
           <div className="col-md-2">
-            <ChartCard
+            <ChartCard2
               title="Users"
               amount="240"
               chartProps={{
@@ -157,7 +256,7 @@ class Acquisition extends React.Component {
             />
           </div>
           <div className="col-md-2">
-            <ChartCard
+            <ChartCard2
               title="Users"
               amount="240"
               chartProps={{
@@ -178,7 +277,7 @@ class Acquisition extends React.Component {
         </div>
         <div className="row">
           <div className="col-md-3">
-            <ChartCard
+            <ChartCard2
               title="Users"
               amount="240"
               chartProps={{
@@ -197,7 +296,7 @@ class Acquisition extends React.Component {
             />
           </div>
           <div className="col-md-3">
-            <ChartCard
+            <ChartCard2
               title="Users"
               amount="240"
               chartProps={{
@@ -221,8 +320,29 @@ class Acquisition extends React.Component {
             </div>
           </div>
         </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <Tabs type="card">
+              <TabPane tab="Language" key="1">
+                <Table columns={columns} dataSource={data} />
+              </TabPane>
+              <TabPane tab="Devices" key="2">
+                <Table columns={columns1} dataSource={data2} />
+              </TabPane>
+              <TabPane tab="Operating System" key="3">
+                <Table columns={columns} dataSource={data} />
+              </TabPane>
+              <TabPane tab="Mobile" key="4">
+                <Table columns={columns} dataSource={data} />
+              </TabPane>
+              <TabPane tab="Browser" key="5 ">
+                <Table columns={columns} dataSource={data} />
+              </TabPane>
+            </Tabs>
+          </div>
+        </div>
       </Authorize>
     )
   }
 }
-export default Acquisition
+export default Audience
